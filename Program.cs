7bd681fab31e966556c83f9a44dece5804617672
Goulart12 <goulartgarcia12@gmail.com>
2025-05-16 21:13:36 -0300

    Desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+var iphone = new Iphone("24900000000", "Iphone 16", "000000000", 128);
+var nokia = new Nokia("24900000000", "Tijolinho", "000000000", 2);
+
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
